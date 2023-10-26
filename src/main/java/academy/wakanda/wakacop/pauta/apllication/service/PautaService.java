c91b232f6af8fe7017297531a1a743c9d72861ee
MARCIO MOURA DOS SANTOS <113490711+Marcioavb@users.noreply.github.com>
2023-10-26 14:46:02 -0300

    valida abertura sessao votacao

--- src/main/java/academy/wakanda/wakacop/pauta/apllication/service/PautaService.java
+++ src/main/java/academy/wakanda/wakacop/pauta/apllication/service/PautaService.java
@@ -2,7 +2,11 @@ package academy.wakanda.wakacop.pauta.apllication.service;
 
 import academy.wakanda.wakacop.pauta.apllication.api.PautaCadastradaResponse;
 import academy.wakanda.wakacop.pauta.apllication.api.novaPautaRequest;
+import academy.wakanda.wakacop.pauta.domain.Pauta;
+
+import java.util.UUID;
 
 public interface PautaService {
     PautaCadastradaResponse cadastraPauta(novaPautaRequest novaPauta);
+    Pauta getPautaPorId(UUID idPauta);
 }
--- src/main/java/academy/wakanda/wakacop/sessaoVotacao/application/service/SessaoVotacaoApplicationService.java
+++ src/main/java/academy/wakanda/wakacop/sessaoVotacao/application/service/SessaoVotacaoApplicationService.java
@@ -1,6 +1,8 @@
 package academy.wakanda.wakacop.sessaoVotacao.application.service;
 
 import academy.wakanda.wakacop.pauta.apllication.repository.PautaRepository;
+import academy.wakanda.wakacop.pauta.apllication.service.PautaService;
+import academy.wakanda.wakacop.pauta.domain.Pauta;
 import academy.wakanda.wakacop.sessaoVotacao.application.api.SessaoAberturaRequest;
 import academy.wakanda.wakacop.sessaoVotacao.application.api.SessaoArbeturaResponse;
 import academy.wakanda.wakacop.sessaoVotacao.application.repository.SessaoVotacaoRepository;
@@ -14,11 +16,13 @@ import org.springframework.stereotype.Service;
 @Log4j2
 public class SessaoVotacaoApplicationService implements SessaoVotacaoService {
     private final SessaoVotacaoRepository sessaoVotacaoRepository;
+    private final PautaService pautaService;
 
     @Override
     public SessaoArbeturaResponse abreSessao(SessaoAberturaRequest sessaoArbeturaRequest) {
         log.info("[inicia] SessaoVotacaoApplicationService - abreSessao");
-        SessaoVotacao sessaoVotacao = sessaoVotacaoRepository.salva(new SessaoVotacao(sessaoArbeturaRequest));
+        Pauta pauta = pautaService.getPautaPorId(sessaoArbeturaRequest.getIdPauta());
+        SessaoVotacao sessaoVotacao = sessaoVotacaoRepository.salva(new SessaoVotacao(sessaoArbeturaRequest, pauta));
         log.info("[finaliza] SessaoVotacaoApplicationService - abreSessao");
         return new SessaoArbeturaResponse(sessaoVotacao);
     }
